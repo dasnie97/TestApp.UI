9cb5a5fd0efdba1c27ce61b4eb218f80982b1f6a
dasnie97 <damiansnieda97@gmail.com>
2022-11-20 18:51:12 +0100

    Removed crashlog table, added Angular Material and NgxMaterial modules to support datetime picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMatDateFormats, NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule, NGX_MAT_DATE_FORMATS } from '@angular-material-components/datetime-picker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogfilesListComponent } from './Components/LogFiles/logfiles-list/logfiles-list.component';
 import { AddLogfileComponent } from './Components/LogFiles/add-logfile/add-logfile.component';
-import { FormsModule } from '@angular/forms';
+
 import { EditLogfileComponent } from './Components/LogFiles/edit-logfile/edit-logfile.component';
 import { CrashlogsComponent } from './Components/crashlogs/crashlogs.component';
 import { MapComponent } from './Components/map/map.component';
 import { ChartComponent } from './Components/chart/chart.component';
+import { DatetimepickerComponent } from './Components/datetimepicker/datetimepicker.component';
+import { NgxMatMomentModule } from '@angular-material-components/moment-adapter';
 
 @NgModule({
   declarations: [
@@ -21,14 +32,28 @@ import { ChartComponent } from './Components/chart/chart.component';
     CrashlogsComponent,
     MapComponent,
     ChartComponent,
+    DatetimepickerComponent,
   ],
   imports: [
+    NgxMatMomentModule,
+    MatCheckboxModule,
+    MatRadioModule,
     BrowserModule,
+    MatSelectModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    NgxMatTimepickerModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
