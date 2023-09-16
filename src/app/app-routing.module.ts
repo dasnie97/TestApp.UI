ea8382c07de059275bb8e983d40574f902588f2a
Damian Śnieda <damiansnieda97@gmail.com>
2023-09-16 18:49:22 +0200

    Removed not used test component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DowntimesComponent } from './Components/downtimes/downtimes.component';
 import { HomeComponent } from './Components/home/home.component';
-import { AutocompleteRequireSelectionExample } from './Components/autocomplete-require-selection-example/autocomplete-require-selection-example.component';
 
 const routes: Routes = [
   {
@@ -15,11 +14,6 @@ const routes: Routes = [
     component:DowntimesComponent,
     data:{animation: 'isRight'}
   },
-  {
-    path: 'test',
-    component:AutocompleteRequireSelectionExample,
-    data:{animation: 'isRight'}
-  }
 ];
 
 @NgModule({
